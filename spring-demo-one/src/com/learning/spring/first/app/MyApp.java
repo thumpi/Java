0e6ec8a75566e8e76d4f4ddb8b3251debbbec10a
Abhijith Dev S <subhash.abhijith.dev@gmail.com>
2020-03-27 00:18:35 +0530

    annotation with component having default id.

--- spring-demo-one/src/com/learning/spring/first/app/MyApp.java
+++ spring-demo-one/src/com/learning/spring/first/app/MyApp.java
@@ -4,12 +4,13 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyApp {
 
+	
 	public static void main(String[] args) {
 			
 		//load the spring configuration file
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		// retrieve bean from spring container
-		Coach theCoach = context.getBean("myTennisCoach",Coach.class);		
+		Coach theCoach = context.getBean("tennisCoach",Coach.class);		
 		
 		//call methods on bean
 		System.out.println(theCoach.getDailyWorkout());
--- spring-demo-one/src/com/learning/spring/first/app/TennisCoach.java
+++ spring-demo-one/src/com/learning/spring/first/app/TennisCoach.java
@@ -2,7 +2,7 @@ package com.learning.spring.first.app;
 
 import org.springframework.stereotype.Component;
 
-@Component("myTennisCoach")
+@Component
 public class TennisCoach implements Coach {
 
 	@Override
