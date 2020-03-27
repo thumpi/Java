9b20628e2342b8293c54ac4f8341ce5754e06be3
Abhijith Dev S <subhash.abhijith.dev@gmail.com>
2020-03-27 23:11:14 +0530

    life cycle with annotation

--- spring-demo-one/src/com/learning/spring/first/app/MyApp.java
+++ spring-demo-one/src/com/learning/spring/first/app/MyApp.java
@@ -10,10 +10,12 @@ public class MyApp {
 		//load the spring configuration file
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		// retrieve bean from spring container
-		Coach theCoach = context.getBean("tennisCoach",Coach.class);		
+		Coach theCoach1 = context.getBean("tennisCoach",Coach.class);
+		Coach theCoach2 = context.getBean("tennisCoach",Coach.class);		
 		
 		//call methods on bean
-		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(theCoach1);
+		System.out.println(theCoach2);
 				//close the context
 		context.close();
 		
--- spring-demo-one/src/com/learning/spring/first/app/TennisCoach.java
+++ spring-demo-one/src/com/learning/spring/first/app/TennisCoach.java
@@ -3,8 +3,11 @@ package com.learning.spring.first.app;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+@Scope("prototype")
+//@Scope("singleton")
 @Component
 public class TennisCoach implements Coach {
 	//
