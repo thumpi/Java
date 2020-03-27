15776facdb220b72d5b73c6da092c3b99da6ff81
Abhijith Dev S <subhash.abhijith.dev@gmail.com>
2020-03-27 21:22:11 +0530

    injectin property file using java

--- spring-demo-one/src/com/learning/spring/first/app/TennisCoach.java
+++ spring-demo-one/src/com/learning/spring/first/app/TennisCoach.java
@@ -2,11 +2,17 @@ package com.learning.spring.first.app;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
 	//
+	@Value("${bla.email}")
+	private String email;
+	
+	@Value("${bla.team}")
+	private String team;
 	private FortuneService fortuneService;
 	
 	@Autowired
@@ -17,7 +23,7 @@ public class TennisCoach implements Coach {
 
 	@Override
 	public String getDailyWorkout() {
-		return("Practice for two hours " + this.fortuneService.getFortune());
+		return("Practice for two hours " + email + team +  this.fortuneService.getFortune());
 	}
 
 	@Override
