b2aa0ea4b2047bc1f505165eb41cf016b7ae069e
Abhijith Dev S <subhash.abhijith.dev@gmail.com>
2020-03-27 17:37:10 +0530

    annotation with constructor injection

--- spring-demo-one/src/com/learning/spring/first/app/HappyFortuneService.java
+++ spring-demo-one/src/com/learning/spring/first/app/HappyFortuneService.java
@@ -1,5 +1,8 @@
 package com.learning.spring.first.app;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class HappyFortuneService implements FortuneService {
 
 	@Override
--- spring-demo-one/src/com/learning/spring/first/app/TennisCoach.java
+++ spring-demo-one/src/com/learning/spring/first/app/TennisCoach.java
@@ -1,13 +1,21 @@
 package com.learning.spring.first.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
+	private FortuneService fortuneService;
+	
+@Autowired
+	public TennisCoach(FortuneService fortuneService) {
+		super();
+		this.fortuneService = fortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
-		return("Practice for two hours");
+		return("Practice for two hours" + this.fortuneService.getFortune());
 	}
 
 	@Override
